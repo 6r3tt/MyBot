9e995e5704c1c5a96fd8e8acb4cb322cd27ff8be
6r3tt <24708310+6r3tt@users.noreply.github.com>
2019-03-17 02:46:03 -0600

    MyBot is an annoying little copy machine. Test out ping.

--- index.js
+++ index.js
@@ -16,18 +16,35 @@ const client = new Discord.Client();
 // Ready check. Ready Up!
 client.once('ready', () => {
   console.log('Ready!')
-})
+});
 
 client.on('message', message => {
   console.log(message.content)
-})
+});
 
 // Repeating logic
 bot.on('message', (message) => {
   if (message.author.id !== bot.user.id && message.content){
     message.channel.sendMessage(message.content);
   }
-})
+});
+
+
+// Woah I have to try this.
+bot.on("message", function (msg) {
+  // if message begins with "ping"
+  if (msg.content.indexOf("ping") === 0 || msg.content.indexOf("Ping") === 0) {
+    // send a message to the channel the ping message was sent in.
+    //bot.sendMessage(msg.channel, "pong!");
+    msg.channel.sendMessage("pong fuckers!");
+
+    // alert the console
+    console.log("pong-ed " + msg.author.username);
+  }
+});
+
+
 
 // Broken Login.
 client.login(config.token);
+bot.login(config.token);
