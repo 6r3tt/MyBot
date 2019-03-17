de3f8393762865436e839531138cc08be1de3ccd
6r3tt <24708310+6r3tt@users.noreply.github.com>
2019-03-17 00:46:58 -0600

    Updated index.js added tested logging function.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const Discord = require('discord.js');
 // Remember to point to the correct location.
 const config = require('./.ignore/config.json');
 
+
+// Testing repeating bot
+const Commando = require('discord.js-commando');
+const bot = new Commando.Client();
+
+
 // Create a client object with Discord.
 const client = new Discord.Client();
 
@@ -15,5 +21,13 @@ client.once('ready', () => {
 client.on('message', message => {
   console.log(message.content)
 })
+
+// Repeating logic
+bot.on('message', (message) => {
+  if (message.author.id !== bot.user.id && message.content){
+    message.channel.sendMessage(message.content);
+  }
+})
+
 // Broken Login.
 client.login(config.token);
